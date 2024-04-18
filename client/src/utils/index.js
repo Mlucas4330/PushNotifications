4dbb3aae26196745d2f0ec0aa06bcfef86965cd6
Lucas Medeiros <mlucas4330@gmail.com>
2024-04-18 00:34:11 -0300

    change the url in the front

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const registerServiceWorker = () => {
     navigator.serviceWorker.register('service-worker.js').then(async serviceWorker => {
-        const publicKeyResponse = await axios.get('http://localhost:3000/push/public_key')
+        const publicKeyResponse = await axios.get('https://pushnotifications.onrender.com/push/public_key')
 
         const subscription = await serviceWorker.pushManager.subscribe({
             userVisibleOnly: true,
@@ -10,7 +10,7 @@ const registerServiceWorker = () => {
         })
 
 
-        await axios.post('http://localhost:3000/push/send', {
+        await axios.post('https://pushnotifications.onrender.com/push/send', {
             subscription
         })
     })
